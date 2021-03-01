3f950a885451c19673bb0551e339e73f86c0e4d9
Tamara Robson <tamara.jade.robson@gmail.com>
2021-03-01 15:13:39 +0100

    Changes to the css of the exo Stupid Coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,18 +5,4 @@ class QuestionsController < ApplicationController
   def answer
     @your_message = params[:question]
   end
-
 end
-
-# puts "Hello, I am your coach, what did you want to tell me?"
-
-#     answer = nil
-
-#     while answer != ""
-#       print "> "
-#       message = gets.chomp
-#       answer = coach_answer(message)
-#       puts answer
-#     end
-
-#     puts "Great, see ya!"
